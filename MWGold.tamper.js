62a9bbbea526ae83f93223cb6dcebf9550fe0645
sean <insanityseanboy@gmail.com>
2018-02-01 17:18:39 +1100

    Adding user/tamper files
    Adding user/tamper files

---
+++ MWGold.tamper.js
@@ -0,0 +1,19 @@
+﻿// ==UserScript==
+// @name        MWGold
+// @namespace   http://userscripts.org/s
+// @description Myth-Weavers Gold
+// @include     /^https?://www\.myth-weavers\.com/
+// @exclude     /^https?://www\.myth-weavers\.com/wiki/
+// @exclude     /^https?://www\.myth-weavers\.com/map\.html/ 
+// @version     1.17.7
+// @grant       GM_listValues
+// @grant       GM_deleteValue
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// @require     https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+// @require     MWGold.lib.min.js
+// @downloadURL https://gist.github.com/insanityseanboy/87d529e1d532f5971353f8c228a098a0/raw/MWGold.user.js
+// ==/UserScript==
+
+makeAwesome();
